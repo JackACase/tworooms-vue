9afa41862cc8b70bbfa69122f25f30f5df95cd7a
Jack Case <jackacase@gmail.com>
2019-07-28 15:25:08 -0400

    got the end time for the timer as a moment object from the start time retrieved from the API added Moment to dependencies

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,9 +1,37 @@
 <template>
-    <h1>it's a game</h1>
+  <h1>it's a game</h1>
 </template>
 
 <script>
+import axios from "axios";
+import moment from "moment"
+// import longGameAvailable from "../gamelogic"
 export default {
-    props: ['accessCode']
-}
+  props: ["accessCode"],
+
+  data() {
+      return {
+          endTime: 0,
+          currentRound: 1,
+          remainingRounds: 2
+      }
+  },
+
+  methods: {
+      setTimer(startTime) {
+          this.endTime = moment(startTime)
+          this.endTime.add(this.remainingRounds + 1, 'minutes')
+      }
+  },
+
+  created() {
+    var self = this
+    axios.get(
+      "http://localhost:8000/game?access_code=" + self.accessCode.toUpperCase()
+    ).then(response => {
+        self.endTime = moment(response.data.start_time)
+        self.endTime.add(3, 'minutes')
+    })
+  }
+};
 </script>
