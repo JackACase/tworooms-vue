c543c17a5022a993f285f877fbbd924424ffea4e
Jack Case <jackacase@gmail.com>
2019-08-08 19:56:25 -0400

    added GameMessage component

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,77 +1,86 @@
 <template>
-  <div>
-    <h1>{{message}}</h1>
-    <Timer
-      v-bind:start-time="startTime"
-      v-bind:duration="remainingRounds + 1"
-      v-bind:running="timerRunning"
-      v-on:time-up="message = 'Time up!'"
-    ></Timer>
-    <button @click="startRound" type="button" class="btn btn-primary">Ready!</button>
-  </div>
+    <div>
+        <GameMessage v-bind:game-state="gameState"></GameMessage>
+        <Timer
+            v-bind:start-time="startTime"
+            v-bind:duration="remainingRounds + 1"
+            v-bind:running="timerRunning"
+            v-on:time-up="message = 'Time up!'"
+        ></Timer>
+        <button
+            @click="startRound"
+            type="button"
+            class="btn btn-primary"
+        >Ready!</button>
+    </div>
 </template>
 
 <script>
 import axios from "axios";
 import Timer from "./Timer.vue";
+import GameMessage from "./GameMessage.vue";
 
 export default {
-  interval: null,
+    interval: null,
 
-  props: ["accessCode"],
+    props: ["accessCode"],
 
-  components: {
-    Timer
-  },
+    components: {
+        Timer,
+        GameMessage
+    },
 
-  data() {
-    return {
-      startTime: 0,
-      currentRound: 1,
-      remainingRounds: 2,
-      timerRunning: false,
-      message: ""
-    };
-  },
+    data() {
+        return {
+            startTime: 0,
+            currentRound: 1,
+            remainingRounds: 2,
+            timerRunning: false,
+            gameState: ""
+        };
+    },
 
-  computed: {
-    buttonLabel() {
-      return this.timerRunning ? "Pause" : "Ready!";
+    computed: {
+        buttonLabel() {
+            return this.timerRunning ? "Pause" : "Ready!";
+        }
+    },
+
+    methods: {
+        startRound() {
+            var self = this;
+            var responseData;
+            axios
+                .post("http://localhost:8000/start/", {
+                    access_code: this.accessCode.toUpperCase(),
+                    state: "roundStarted"
+                })
+                .then(response => {
+                    responseData = response.data;
+                    self.startTime = response.data.start_time;
+                    self.gameState = responseData.state;
+                });
+        }
+    },
+
+    created() {
+        var self = this;
+        this.gameState = "pickingLeader";
+        this.interval = setInterval(() => {
+            axios
+                .get(
+                    "http://localhost:8000/game?access_code=" +
+                    self.accessCode.toUpperCase()
+                )
+                .then(response => {
+                    if (response.data.state == "roundStarted") {
+                        this.timerRunning = true;
+                        self.startTime = response.data.start_time;
+                        clearInterval(self.interval);
+                        self.gameState == response.data.state;
+                    }
+                });
+        }, 500);
     }
-  },
-
-  methods: {
-    startRound() {
-      var self = this;
-      var responseData
-      axios
-        .post("http://localhost:8000/start/", {
-          access_code: this.accessCode.toUpperCase(),
-          state: "roundStarted"
-        })
-        .then(response => {
-          responseData = response.data
-          self.startTime = response.data.start_time;
-          self.message = "Round " + responseData.currentRound + " in progress"
-        })
-    }
-  },
-
-  created() {
-    var self = this;
-    this.message = "Select a leader!"
-    this.interval = setInterval(() => {
-      axios.get(
-        "http://localhost:8000/game?access_code=" +
-          self.accessCode.toUpperCase()
-      ).then(response => {
-          if(response.data.state == "roundStarted") {
-              this.timerRunning = true
-              self.startTime = response.data.start_time
-              clearInterval(self.interval)
-          }
-      })
-    }, 500);
-  }
 };
 </script>
---
+++ src/components/GameMessage.vue
@@ -0,0 +1,32 @@
+<template>
+    <h1>{{message}}</h1>
+</template>
+
+<script>
+export default {
+    props: ["gameState"],
+
+    data() {
+        return {
+        }
+    },
+
+    computed: {
+        message: function() {
+            var message
+            switch(this.gameState) {
+                case "roundStarted":
+                    message ="round in progress"
+                    break
+                case "pickingLeader":
+                    message = "select a room leader"
+                    break
+                default:
+                    message = "default"
+            }
+            return message
+        }
+    }
+}
+</script>
+
