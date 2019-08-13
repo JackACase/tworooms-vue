dbf496fdba3c2b19c645b7609d6b669bd7072bde
Jack Case <jackacase@gmail.com>
2019-08-13 19:43:46 -0400

    start on the game card component will need some method of configuration, as well as method for giving each player a different card from a set

---
+++ src/components/GameCard.vue
@@ -0,0 +1,16 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    props: ['card'],
+
+    data() {
+        return {
+            color = "red",
+            name = "red team"
+        }
+    }
+}
+</script>
