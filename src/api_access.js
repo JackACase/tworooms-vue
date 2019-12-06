0a68df9ebc864495b4f8483c1ff753319ee0f8d3
Jack Case <jackacase@gmail.com>
2019-12-06 21:47:18 +0000

    adding more functions to api_access

--- src/api_access.js
+++ src/api_access.js
@@ -2,10 +2,17 @@ import axios from "axios";
 
 const API_BASE = "https://8000-ed3b72c1-001b-4b78-8432-da680fe8be33.ws-us02.gitpod.io/"
 
+/*
+ * Creates a new game with the player as the moderator
+ * returns a promise which will contain the response
+ */
 export function createGame(playername) {
   return axios.post(API_BASE + "game/", { player_name: playername })
 }
 
+/*
+ * Joins an existing game as a player
+ */
 export function joinGame(playername, accesscode) {
   return axios.post(API_BASE + "join/", {
     player_name: playername,
@@ -13,10 +20,24 @@ export function joinGame(playername, accesscode) {
   })
 }
 
+/*
+ * Retrieves a game object by the access code
+ */
 export function getGame(accessCode) {
   return axios.get(API_BASE + "game/", {
     params: {
       access_code: accessCode
+    }
+  })
+}
+
+/*
+ * Retrieves a player object by playerID
+ */
+export function getPlayer(playerID) {
+  return axios.get(API_BASE + "player/", {
+    params: {
+      player_id: playerID
     }
   })
 }
