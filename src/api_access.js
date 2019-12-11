498770498d2dc617b52888dec5755a57067e82f7
Jack Case <jackacase@gmail.com>
2019-12-11 23:12:36 +0000

    formatted api_access

--- src/api_access.js
+++ src/api_access.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from "axios"
 
 const API_BASE = "https://8000-ed3b72c1-001b-4b78-8432-da680fe8be33.ws-us02.gitpod.io/"
 
@@ -7,44 +7,44 @@ const API_BASE = "https://8000-ed3b72c1-001b-4b78-8432-da680fe8be33.ws-us02.gitp
  * returns a promise which will contain the response
  */
 export function createGame(playername) {
-  return axios.post(API_BASE + "game/", { player_name: playername })
+    return axios.post(API_BASE + "game/", { player_name: playername })
 }
 
 /*
  * Joins an existing game as a player
  */
 export function joinGame(playername, accesscode) {
-  return axios.post(API_BASE + "join/", {
-    player_name: playername,
-    access_code: accesscode.toUpperCase()
-  })
+    return axios.post(API_BASE + "join/", {
+        player_name: playername,
+        access_code: accesscode.toUpperCase()
+    })
 }
 
 /*
  * Retrieves a game object by the access code
  */
 export function getGame(accessCode) {
-  return axios.get(API_BASE + "game/", {
-    params: {
-      access_code: accessCode
-    }
-  })
+    return axios.get(API_BASE + "game/", {
+        params: {
+            access_code: accessCode
+        }
+    })
 }
 
 export function updateGameState(state, accessCode) {
-  return axios.post(API_BASE + "gamestate/", {
-    access_code: accessCode.toUpperCase(),
-    state: state
-  })
+    return axios.post(API_BASE + "gamestate/", {
+        access_code: accessCode.toUpperCase(),
+        state: state
+    })
 }
 
 /*
  * Retrieves a player object by playerID
  */
 export function getPlayer(playerID) {
-  return axios.get(API_BASE + "player/", {
-    params: {
-      player_id: playerID
-    }
-  })
+    return axios.get(API_BASE + "player/", {
+        params: {
+            player_id: playerID
+        }
+    })
 }
