7dfa687c7e29b7f52ede2eb887e42e0390f61c41
Jack Case <jackacase@gmail.com>
2020-02-16 14:36:54 +0000

    working on game card component

--- src/components/GameCard.vue
+++ src/components/GameCard.vue
@@ -1,10 +1,16 @@
 <template>
-    
+    <div>
+        <h1>{{card.name}}</h1>
+        <img v-bind:src="card.image" alt="no image">
+        <p>{{card.description}}</p>
+    </div>
 </template>
 
 <script>
 export default {
-    props: ['card'],
+    props: {
+        card: Object
+    },
 
     data() {
         return {
